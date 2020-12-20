d4bc9e04ba162738e41ebde99ebd4f0ce53800dc
Huang ChenGang <hcggang@163.com>
2020-12-20 13:51:52 +0800

    创建项目

---
+++ src/main/java/com/jn/universal/JnUniversalApplication.java
@@ -0,0 +1,13 @@
+package com.jn.universal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JnUniversalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JnUniversalApplication.class, args);
+    }
+
+}
